c89d3459c3311d8b77509dda0c16e3bfe15422da
KyoAzami <kyotaro.azami@gmail.com>
2024-09-16 13:25:51 -0600

    Se ha agregado la pagina acerca de y se han modificado los enlaces

---
+++ JavaScript/infotext.js
@@ -0,0 +1,22 @@
+// Selecciona el botón y el section
+const toggleButton = document.getElementById('b2');
+const section = document.getElementById('box1');
+
+// Verifica si el section estaba visible en la última sesión
+if (localStorage.getItem('sectionVisible') === 'true') {
+    section.classList.remove('hidden');
+    section.classList.add('visible');
+}
+
+// Agrega un evento para desplegar o retraer el section con animación
+toggleButton.addEventListener('click', function() {
+    if (section.classList.contains('hidden')) {
+        section.classList.remove('hidden');
+        section.classList.add('visible');
+        localStorage.setItem('sectionVisible', 'true'); // Guarda el estado en localStorage
+    } else {
+        section.classList.remove('visible');
+        section.classList.add('hidden');
+        localStorage.setItem('sectionVisible', 'false'); // Actualiza el estado en localStorage
+    }
+});
